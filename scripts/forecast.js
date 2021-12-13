39a772194106bab32f31609e4119862e33153b6b
vosborg <56126813+vosborg@users.noreply.github.com>
2021-12-13 19:47:06 +0100

    Add files via upload

---
+++ scripts/forecast.js
@@ -0,0 +1,27 @@
+
+const key = 'CQJnEfEDogXlTWeCGslRc4sAgiueCjEH';
+
+// Get weather information
+// async --> because we make a request and we that to return a promise
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+// Get City Information
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
